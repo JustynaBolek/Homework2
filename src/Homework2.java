d218dbb571c76caed1ef8044894179986b4ba3ee
JustynaBolek <justyna.bolek@gmail.com>
2022-09-20 21:14:43 +0200

    added a string name and surname added if/else

---
+++ src/Homework2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        System.out.println("What's your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("What's your surname?");
+        String surname = scanner.nextLine();
+        System.out.println("How old are you?");
+        int x = scanner.nextInt();
+        if (x>18){
+            System.out.println(name + " " + surname + " you can come in");
+        }else {
+            System.out.println(name + " " + surname + " you are too young to come in");
+        }
+    }
+
+}
