85f9646ce3ffd0000177bb1224403fea811d67ad
JustynaBolek <justyna.bolek@gmail.com>
2022-09-21 13:35:26 +0200

    added a string Happy Hours added for loop

--- src/Homework2.java
+++ src/Homework2.java
@@ -5,15 +5,23 @@ public class Homework2 {
         System.out.println("What's your name?");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.println("What's your surname?");
-        String surname = scanner.nextLine();
         System.out.println("How old are you?");
         int x = scanner.nextInt();
-        if (x>18){
-            System.out.println(name + " " + surname + " you can come in");
+        if (x>=18){
+            System.out.println(name + " " + " you can come in");
+            if (x>=18&25>=x) {
+                System.out.println("You have free beer");
+                String[] happyHours = new String[]{"Justyna", "Alicja", "Hania", "Ania", "Emilia"};
+                for(String person:happyHours){
+                    if(name.equals(person)){
+                        System.out.println("Oh, you are also on the Happy Hour list and you get a free shot!");
+                    }
+                }
+            } else {
+                System.out.println("Sorry you are too old to get a free beer");
+            }
         }else {
-            System.out.println(name + " " + surname + " you are too young to come in");
+            System.out.println(name + " " + " you are too young to come in");
         }
     }
-
 }
